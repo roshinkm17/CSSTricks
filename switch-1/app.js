92d36a8cd712b0065fa1832d1b2e15fa6686183f
Roshin K M <roshinkm17@gmail.com>
2023-06-24 01:28:29 +0530

    Added outer shell

--- switch-1/app.js
+++ switch-1/app.js
@@ -1,6 +1,12 @@
 const button = document.querySelector(".switch_btn");
 const btnWrapper = document.querySelector(".switch_wrapper");
+const bird = document.querySelectorAll(".bird");
 
 button.addEventListener("click", () => {
   btnWrapper.classList.toggle("active");
+  bird.forEach((item) => {
+    btnWrapper.classList.contains("active")
+      ? (item.style.animation = "bird-exit 2s ease-in-out forwards")
+      : (item.style.animation = "bird-enter 2s ease-in-out infinite alternate");
+  });
 });
